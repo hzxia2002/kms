b8607d95d50448b50ee679c09926a7018f5f654d
tianchungang <tianchungang@gmail.com>
2014-02-27 23:42:42 +0800

    ..

--- src/com/comet/system/manager/UserManager.java
+++ src/com/comet/system/manager/UserManager.java
@@ -1,10 +1,9 @@
 package com.comet.system.manager;
 
 import com.comet.system.daoservice.SysPrivilegeService;
-import com.comet.system.daoservice.SysRoleService;
 import com.comet.system.daoservice.SysUserService;
+import com.comet.system.domain.SysPerson;
 import com.comet.system.domain.SysPrivilege;
-import com.comet.system.domain.SysRole;
 import com.comet.system.domain.SysUser;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -21,107 +21,91 @@ import java.util.List;
  */
 @Service
 public class UserManager {
-	@Autowired
-	private SysUserService sysUserService;
+    @Autowired
+    private SysUserService sysUserService;
 
     @Autowired
-    private SysRoleService sysRoleService;
-
-	@Autowired
-	private SysPrivilegeService sysPrivilegeService;
-
-	/**
-	 * 通过属性进行查询
-	 *
-	 * @param propertyName
-	 *            属性名
-	 * @param value
-	 *            属性值
-	 * @return 用戶列表
-	 */
-	public List<SysUser> findByProperty(String propertyName, String value) {
-		return sysUserService.findByProperty(propertyName, value);
-	}
-
-	/**
-	 * 取得用户角色授权权限
-	 *
-	 * @param user
-	 *            登录用户
-	 * @param isDeny
-	 *            是否拒绝
-	 * @return 权限列表
-	 */
-	public List<SysPrivilege> getUserRolePrivileges(SysUser user, String isDeny) {
-		String sql = "from SysPrivilege t";
-//				+ " where t.id in (select privilege "
-//				+ " from SysRolePrivilege "
-//				+ " where role in (select role from SysUserRole where user = "
-//				+ user.getId() + ")) ";
-
-//		if (StringUtils.isNotEmpty(isDeny)) {
-//			sql += " and t.id not in (select privilege from SysUserPrivilege where user = "
-//					+ user.getId() + " and isDeny = " + isDeny + ")";
-//		}
-		List<SysPrivilege> list = sysPrivilegeService.find(sql);
-		List<SysPrivilege> ret = new ArrayList<SysPrivilege>();
-
-		for (SysPrivilege tmp : list) {
-			tmp.setPrivilegeType();
-
-			ret.add(tmp);
-		}
-
-		return ret;
-	}
-
-	/**
-	 * 取得用户拥有的权限
-	 *
-	 * @param user
-	 *            登录用户
-	 * @param isDeny
-	 *            是否拒绝
-	 * @return 权限列表
-	 */
-	@SuppressWarnings("unchecked")
-	public List<SysPrivilege> getUserPrivileges(SysUser user, String isDeny) {
-		List<SysPrivilege> ret = new ArrayList();
-		String sql = "from SysPrivilege t "   ;
-//				+ "where t.id = t2.privilege ";
-
-//		if (StringUtils.isNotEmpty(isDeny)) {
-//			sql += " and t2.isDeny = " + isDeny;
-//		}
-//
-//		sql += " and t2.user in (select id from SysUser "
-//				+ "where loginName = '" + user.getLoginName() + "')";
-
-		List list = sysPrivilegeService.find(sql);
-
-		if (list != null && list.size() > 0) {
-			for (int i = 0; i < list.size(); i++) {
-//				Object[] obj = (Object[]) list.get(i);
-				SysPrivilege p = (SysPrivilege) list.get(i);
-
-				p.setPrivilegeType();
-
-				ret.add(p);
-			}
-		}
-
-		return ret;
-	}
+    private SysPrivilegeService sysPrivilegeService;
 
     /**
-     * 取得当前用户的角色
+     * 通过属性进行查询
+     *
+     * @param propertyName
+     *            属性名
+     * @param value
+     *            属性值
+     * @return 用戶列表
+     */
+    public List<SysUser> findByProperty(String propertyName, String value) {
+        return sysUserService.findByProperty(propertyName, value);
+    }
+
+    /**
+     * 取得用户角色授权权限
      *
      * @param user
-     * @return
+     *            登录用户
+     * @param isDeny
+     *            是否拒绝
+     * @return 权限列表
      */
-    public List<SysRole> getUserRoles(SysUser user) {
-        String sql = "select t.role from SysUserRole t where t.user.id = " + user.getId();
+    public List<SysPrivilege> getUserRolePrivileges(SysUser user, String isDeny) {
+        String sql = "from SysPrivilege t"
+                + " where t.id in (select privilege "
+                + " from SysRolePrivilege "
+                + " where role in (select role from SysUserRole where user = "
+                + user.getId() + ")) ";
 
-        return sysRoleService.find(sql);
+        if (StringUtils.isNotEmpty(isDeny)) {
+            sql += " and t.id not in (select privilege from SysUserPrivilege where user = "
+                    + user.getId() + " and isDeny = " + isDeny + ")";
+        }
+        List<SysPrivilege> list = sysPrivilegeService.find(sql);
+        List<SysPrivilege> ret = new ArrayList<SysPrivilege>();
+
+        for (SysPrivilege tmp : list) {
+            tmp.setPrivilegeType();
+
+            ret.add(tmp);
+        }
+
+        return ret;
+    }
+
+    /**
+     * 取得用户拥有的权限
+     *
+     * @param user
+     *            登录用户
+     * @param isDeny
+     *            是否拒绝
+     * @return 权限列表
+     */
+    @SuppressWarnings("unchecked")
+    public List<SysPrivilege> getUserPrivileges(SysUser user, String isDeny) {
+        List<SysPrivilege> ret = new ArrayList();
+        String sql = "from SysPrivilege t, SysUserPrivilege t2 "
+                + "where t.id = t2.privilege ";
+
+        if (StringUtils.isNotEmpty(isDeny)) {
+            sql += " and t2.isDeny = " + isDeny;
+        }
+
+        sql += " and t2.user in (select id from SysUser "
+                + "where loginName = '" + user.getLoginName() + "')";
+
+        List list = sysPrivilegeService.find(sql);
+
+        if (list != null && list.size() > 0) {
+            for (int i = 0; i < list.size(); i++) {
+                Object[] obj = (Object[]) list.get(i);
+                SysPrivilege p = (SysPrivilege) obj[0];
+                p.setPrivilegeType();
+
+                ret.add(p);
+            }
+        }
+
+        return ret;
     }
 }
